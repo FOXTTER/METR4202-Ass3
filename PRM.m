53b687fec638bbcf0aab21561b9a155a1d34114f
Christian Witting <witting.chr@gmail.com>
2015-10-23 14:15:54 +1000

    Working PRM

--- PRM.m
+++ PRM.m
@@ -1,34 +1,46 @@
 clear
 raw = imread('hand_mask.png');
 hand = rgb2gray(raw) > 10;
-n = 20;
-samples = zeros(n+2,2);
+n = 200;
 start = [1210 10];
 stop = [1710 1040];
-samples(1,:) = start;
-samples(2,:) = stop;
+figure
+imshow(hand);
+[x, y] = getpts;
+start(1) = x(1);
+start(2) = y(1);
+stop(1) = x(2);
+stop(2) = y(2);
+samples(1) = struct('pt', start, 'pre', []);
+samples(2) = struct('pt', stop, 'pre', []);
+%%
 for i = 3 : n
     x = round(random('unif',1, size(hand,2)));
     y = round(random('unif',1, size(hand,1)));
-    samples(i,:) = [x y];
+    samples(i) = struct('pt',[x y], 'pre',[]);
 end
 %[hand, out] = hasCollision([1210 10], [1710 1040], hand);
 %disp(out);
 %
-%figure
-%imshow(hand);
-%hold on
-%plot([1210 1710], [10 1040], 'o');
-%hold off
 %%
 Cmatrix = zeros(n);
 for i = 1 : n
-    for k = 1 : n
+    for k = i : n
         if (i ~= k)
-            Cmatrix(i,k) = (hasCollision(samples(i,:), samples(k,:), hand) == 0);
+            if (hasCollision(samples(i).pt, samples(k).pt, hand) == 0) 
+                Cmatrix(i,k) = norm(samples(i).pt - samples(k).pt);
+                Cmatrix(k,i) = Cmatrix(i,k);
+            end
         end
     end
 end
-graph = biograph(Cmatrix);
-current = sample(1,:);
-for i = 1 : n
+CM = sparse(Cmatrix);
+%%
+[dist, path, pred] = graphshortestpath(CM, 1, 2);
+imshow(hand);
+hold on
+prev = path(1);
+for i = 1 : length(path)
+    plot([samples(prev).pt(1) samples(path(i)).pt(1)], [samples(prev).pt(2) samples(path(i)).pt(2)], 'b');
+    prev = path(i);
+end
--- hasCollision.m
+++ hasCollision.m
@@ -1,21 +1,29 @@
 function [out ] = hasCollision(pt1, pt2, image)
-    out = false;
+    out = true;
     dp = max(abs(pt2(1)-pt1(1)), abs(pt2(2)-pt1(2)));
+    dp = dp*10;
     delta_x = (pt2(1)-pt1(1));
     delta_y = (pt2(2)-pt1(2));
     dx = delta_x/dp;
     dy = delta_y/dp;
-    x = pt1(1);
-    y = pt1(2);
+    x1 = pt1(1);
+    y1 = pt1(2);
+    x2 = pt2(1);
+    y2 = pt2(2);
     %hand = image;
-    while(abs(x - pt2(1)) > 2 || abs(y - pt2(2)) > 2)
-        if (image(round(y),round(x)) == 1)
+    if (image(floor(min(y1,y2)):floor(max(y1,y2)),floor(min(x1,x2)):floor(max(x1,x2))) == 0)
+        out = false;
+        return;
+    end
+    out = false;
+    while(abs(x1 - x2) > 2 || abs(y1 - y2) > 2)
+        if (image(round(y1),round(x1)) == 1)
             out = true;
             return;
         end
         %hand(round(y),round(x)) = 1;
-        x = x + dx;
-        y = y + dy;
+        x1 = x1 + dx;
+        y1 = y1 + dy;
     end
 end
 
